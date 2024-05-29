f0eba184ce035c98c44933f92ca97cc4b8c3d543
Simo Khanyi <simokhanyi@gmail.com>
2024-05-29 02:36:13 +0000

    Flask application

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 
 
 class Config:
+    """
+    Configuration class for the Flask app.
+
+    Attributes:
+        LANGUAGES (list): List of supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -20,11 +28,23 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """
+    Get the best matching language for the user's locale.
+
+    Returns:
+        str: Best matching language code.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    """
+    Render the index.html template.
+
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template('2-index.html')
 
 
